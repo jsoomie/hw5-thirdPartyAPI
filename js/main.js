7cec22c529935c257dc45f83794c1a1c67c4f550
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-11 16:47:46 -0800

    added current time in header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 
 $(document).ready(function() {
     console.log("Hello World!");
+
+    // Displays Current Time in header
+    setInterval(currentTime, 1000);
+    function currentTime() {
+        $("#header-current-time").text(moment());
+    } 
+
+    
 })
