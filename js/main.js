d87ed899a929b836dfffd614da83082ebf7bf0eb
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-11 17:24:11 -0800

    added stylings for past present future. found a way to grab the value of the hour, not yet implemented.

--- js/main.js
+++ js/main.js
@@ -8,5 +8,30 @@ $(document).ready(function() {
         $("#header-current-time").text(moment());
     } 
 
-    
+    function hourCheck() {       
+        // check what the hour is to compare against
+        let hourNow = moment().hours();
+        console.log(hourNow);
+
+        $(".hour-container").each(function() {
+            let selectedHour = parseInt($(this).val());
+            if (selectedHour < hourNow) {
+                $(".col-md-10").addClass("past");
+            } else if (selectedHour === hourNow) {
+                $(".col-md-10").removeClass("past");
+                $(".col-md-10").addClass("present");
+            } else {
+                $(".col-md-10").removeClass("past");
+                $(".col-md-10").removeClass("present");
+                $(".col-md-10").addClass("future");
+            }
+
+        })
+
+        console.log(parseInt($(".hour-container").attr('value')));
+
+
+    }
+
+    hourCheck();
 })
